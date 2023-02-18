56f0f71350178158dd2fcd0a0a7a6de8f4236c2a
M4rrix <mariacamana998@gmail.com>
2023-02-18 13:54:41 -0300

    Maria camaña
    cv

---
+++ js/function.js
@@ -0,0 +1,36 @@
+function myFunction() {
+  alert("CV MARIA CAMAÑA");
+}
+
+function mouseOver () {
+  document.getElementById("h1").innerHTML="style.color='#04FAE9"
+}
+
+function mouseOut () {
+  document.getElementById("h1").innerHTML="style.color='black"
+}
+
+
+document.getElementById('boton-whatsapp') .addEventListener('click', function(){
+  document.getElementById("demo").innerHTML = "+54(2920)-308378"
+});
+
+document.getElementById('boton-email') .addEventListener('click', function(){
+  document.getElementById("demo").innerHTML = "mariacamana998@gmail.com"
+});
+
+document.getElementById('boton-direccion') .addEventListener('click', function(){
+document.getElementById("demo").innerHTML = "Chacarita, Buenos Aires"
+});
+
+
+
+
+
+
+
+
+
+
+
+  
